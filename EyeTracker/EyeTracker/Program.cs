56abaf024a708acd503a24e6d90fe88c80cfb751
Corey Rigney <coreyrigney@gmail.com>
2019-04-02 19:41:11 -0400

    Set up Calibration. Fixed most remaining bugs

--- EyeTracker/EyeTracker/Program.cs
+++ EyeTracker/EyeTracker/Program.cs
@@ -11,18 +11,38 @@ namespace EyeTracker
     {
         static void Main(string[] args)
         {
-            
-            bool quit = false;
-            int count = 0;
+            /*
+             *Setup HTTP libraries 
+             */
+            Console.WriteLine("Welcome to the Eye Tracker test console. To calibrate for screens, type 'c'.");
+            Console.WriteLine("To calibrate for any car, type 'car'.");
+
+            //The instantiated tracker object handles everything below the surface.
             Tracking tracker = new Tracking();
-            //tracker.Calibrate();
+
+            
+
+            string input = Console.ReadLine();
+
+            if (input == "c")
+            {
+                //Calibrate the tracker using the screen calibration method
+                tracker.Calibrate();
+            }
+            else if(input == "car")
+            {
+                tracker.CarCalibrate();
+            }
+            else
+            {
+                //Run the tracker in uncalibrated mode
+                tracker.gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => tracker.UncalibratedZoneTracker(gazePointX, gazePointY));
+            }
+
             tracker.gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => tracker.ZoneTracker(gazePointX, gazePointY));
 
+            //Without this line, the tracker doesn't collect input. 
             Console.ReadLine();
-               
-           
-
-            
             
             
 
--- EyeTracker/EyeTracker/Tracking.cs
+++ EyeTracker/EyeTracker/Tracking.cs
@@ -9,86 +9,95 @@ namespace EyeTracker
 {
     class Tracking
     {
-        public List<Zone> zoneList;
-        public double xLoc;
-        public double yLoc;
-        public bool zoneReturn;
+        public Dictionary<string, Zone> zoneList;
         Host host = new Host();
         public GazePointDataStream gazePointDataStream;
 
+        /// <summary>
+        /// Initialize the tracker with a new GazePointStream, which must be initialized for the tracker to collect data
+        /// </summary>
         public Tracking()
         {
             this.gazePointDataStream = this.host.Streams.CreateGazePointDataStream();
-            this.zoneReturn = false;
-            this.zoneList = new List<Zone>();
-        }
-
-        /*
-        public double EyeX()
-        {
-            var gazePointDataStream = host.Streams.CreateGazePointDataStream();
-            double x = 0;
-            gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => x = gazePointX);
-            this.xLoc = x;
-            return x;
-        }
-
-        public double EyeY()
-        {
-            
-            var gazePointDataStream = host.Streams.CreateGazePointDataStream();
-            double y = 0;
-            gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => y = gazePointY);
-            this.yLoc = y;
-            return y;
-        }
-        */
-
-        public bool InZone(Zone zone)
-        {
-
-            this.gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => ZoneTracker(gazePointX, gazePointY));
-            return this.zoneReturn;
+            this.zoneList = new Dictionary<string, Zone>();
         }
 
         public void ZoneTracker(double x, double y)
         {
-            foreach(Zone zone in this.zoneList)
+            foreach(KeyValuePair<string, Zone> zone in this.zoneList)
             {
-                if ((x >= zone.X && x <= zone.Width) && (y >= zone.Y && y <= zone.Height))
+                if ((x >= zone.Value.X && x <= zone.Value.Width) && (y >= zone.Value.Y && y <= zone.Value.Height))
                 {
-                    Console.WriteLine($"Eyes looking at Zone {zone.Name}");
-                    this.zoneReturn = true;
+                    Console.WriteLine($"Eyes looking at Zone {zone.Value.Name}");
                 }
             }
-            
+        }
+
+        public void UncalibratedZoneTracker(double x, double y)
+        {
+            Console.WriteLine("Eyes currently looking at Point {" + x + ", " + y + "}");
         }
 
         public void NewZone(string name, int x, int y, int width, int height)
         {
-            Zone newZone = new Zone(name, x, y, width, height);
-            zoneList.Add(newZone);
+            if (!zoneList.ContainsKey(name))
+            {
+                Zone newZone = new Zone(name, x, y, width, height);
+                zoneList.Add(name, newZone);
+                Console.WriteLine($"New Zone {name} added at {x}, {y} with a width {width} and a height of {height}");
+            }
+            
+            
         }
 
         public void Calibrate()
         {
+            zoneList.Clear();
             Console.WriteLine("Please look at the center of your screen.");
-            for(int i = 0; i < 100; i++)
-            {
-                //Wait
-            }
-            var gazePointDataStream = host.Streams.CreateGazePointDataStream();
-            //gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => NewZone("center", (int)gazePointX - 200, (int)gazePointY - 200, 400, 400));
+            System.Threading.Thread.Sleep(5000);
+            this.gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => NewZone("center", (int)gazePointX - 200, (int)gazePointY - 200, 400, 400));
             Console.WriteLine();
 
             Console.WriteLine("Please look at the top of your screen.");
-            for (int i = 0; i < 100; i++)
-            {
-                //Wait
-            }
-            //gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => NewZone("top", (int)gazePointX - 200, (int)gazePointY - 200, 400, 400));
+            System.Threading.Thread.Sleep(5000);
+            this.gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => NewZone("top", (int)gazePointX - 200, (int)gazePointY - 200, 400, 200));
             Console.WriteLine();
 
+            Console.WriteLine("Please look at the left of your screen.");
+            System.Threading.Thread.Sleep(5000);
+            this.gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => NewZone("left", (int)gazePointX - 200, (int)gazePointY - 200, 200, 400));
+            Console.WriteLine();
+
+            Console.WriteLine("Please look at the bottom of your screen.");
+            System.Threading.Thread.Sleep(5000);
+            this.gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => NewZone("bottom", (int)gazePointX - 200, (int)gazePointY - 200, 400, 200));
+            Console.WriteLine();
+
+            Console.WriteLine("Please look at the right of your screen.");
+            System.Threading.Thread.Sleep(5000);
+            this.gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => NewZone("right", (int)gazePointX - 200, (int)gazePointY - 200, 200, 400));
+            Console.WriteLine();
+
+            Console.WriteLine("Calibration for screens completed.");
+            Console.WriteLine();
+        }
+
+        public void CarCalibrate()
+        {
+            zoneList.Clear();
+            Console.WriteLine("Please look at the center of the steering wheel.");
+            System.Threading.Thread.Sleep(1500);
+            this.gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => NewZone("Wheel", (int)gazePointX - 300, (int)gazePointY - 300, 600, 600));
+            Console.WriteLine();
+
+            Console.WriteLine("Please look at the center of the windshield.");
+            System.Threading.Thread.Sleep(1500);
+            this.gazePointDataStream.GazePoint((gazePointX, gazePointY, _) => NewZone("Windshield", (int)gazePointX - 400, (int)gazePointY - 800, 800, 1600));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Calibration for Car completed.");
+            Console.WriteLine();
         }
     }
 }
